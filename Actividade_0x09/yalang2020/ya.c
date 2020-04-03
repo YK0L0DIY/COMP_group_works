80ab8c5937306b7982d8c46bb8ff8b3b05c3aca2
Edfame <theedfame@gmail.com>
2020-04-03 17:06:45 +0100

    fixed ya.* directories

--- Actividade_0x09/yalang2020/ya.c
+++ Actividade_0x09/yalang2020/ya.c
@@ -4,15 +4,14 @@ t_decls t_decls_new(t_decl decl, t_decls decls) {
     t_decls to_return = (t_decls*) malloc(sizeof(struct t_decls_));
 
     to_return->u.decl = decl;
+    to_return->u.decls = decls;
 
     if (decls) {
 
-        to_return->u.decls = decls;
         to_return->kind = DECLS_LIST;
 
     } else {
 
-        to_return->u.decls = NULL;
         to_return->kind = DECLS_SINGLE;
 
     }
